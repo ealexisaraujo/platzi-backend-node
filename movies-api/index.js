4a7743a3dcf64d6e80b291e6ea12f2bec89af184
Alexis Araujo <ala1289@gmail.com>
2019-10-17 10:50:04 -0500

    Reto año bisiesto

--- movies-api/index.js
+++ movies-api/index.js
@@ -7,6 +7,15 @@ app.get('/', function(req, res) {
   res.send('hello world');
 });
 
+app.get('/leap-year/:year', (req, res) => {
+  const year = parseInt(req.params.year);
+  if ((year % 4 === 0 && year & (100 !== 0)) || year % 400 === 0) {
+    res.send('Is leap-year');
+  } else {
+    res.send('Is not leap-year');
+  }
+});
+
 app.get('/json', function(req, res) {
   res.json({ hello: 'world' });
 });
