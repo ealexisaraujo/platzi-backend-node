b665776aba304a68d7629beac71d87f151d02eca
Alexis Araujo <ala1289@gmail.com>
2019-10-17 12:34:02 -0500

    Implementacion un CRUD en Express.js

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,23 +2,9 @@ const express = require('express');
 const app = express();
 
 const { config } = require('./config/index');
+const movieApi = require('./routes/movies.js');
 
-app.get('/', function(req, res) {
-  res.send('hello world');
-});
-
-app.get('/leap-year/:year', (req, res) => {
-  const year = parseInt(req.params.year);
-  if ((year % 4 === 0 && year & (100 !== 0)) || year % 400 === 0) {
-    res.send('Is leap-year');
-  } else {
-    res.send('Is not leap-year');
-  }
-});
-
-app.get('/json', function(req, res) {
-  res.json({ hello: 'world' });
-});
+movieApi(app);
 
 app.listen(config.port, function() {
   console.log(`Listening http://localhost:${config.port}`);
